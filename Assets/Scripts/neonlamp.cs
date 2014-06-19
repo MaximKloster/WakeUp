7192d8b513a9d1d1db56ad50674c2af2a2e02a5c
MarkusWeigel <markusandreasweigel@gmail.com>
2014-06-19 12:37:38 +0200

    lampen bastelei2

---
+++ Assets/Scripts/neonlamp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class neonlamp : MonoBehaviour
+{
+    public bool flackern = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        light.active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (flackern) {
+            light.active = false;
+            randomwait();
+            light.active = true;
+
+        }
+
+
+    }
+
+    IEnumerator randomwait()
+    {
+         yield return new WaitForSeconds(Random.value);
+
+    }
+}
