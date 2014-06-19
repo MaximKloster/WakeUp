adc43e26f78403163680554a133a4b343efd1c2e
MaximKloster <kloster.maxim@web.de>
2014-06-19 13:33:12 +0200

    Sound Event Prefab + Test_Level Update

---
+++ Assets/Scripts/Event Scripts/EventTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTrigger : MonoBehaviour 
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") transform.parent.GetComponent<EventController>().ActivateEvent();
+    }
+}
