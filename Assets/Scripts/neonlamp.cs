c83880ba43110940e7a8e47e52a8967aa5159ef9
MaximKloster <kloster.maxim@web.de>
2014-06-19 15:26:27 +0200

    test level modifired + neonlamp script fixed

--- Assets/Scripts/neonlamp.cs
+++ Assets/Scripts/neonlamp.cs
@@ -4,27 +4,25 @@ using System.Collections;
 public class neonlamp : MonoBehaviour
 {
     public bool flackern = false;
-	public bool on = true;
+    public bool on = true;
     // Use this for initialization
     void Start()
     {
-		light.enabled = on;  
+        light.enabled = on;
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (flackern) {
-						if (Random.value > 0.9) { //a random chance
-								if (light.enabled == true) { //if the light is on...
-										light.enabled = false; //turn it off
-								} else {
-										light.enabled = true; //turn it on
-								}
-						}
+        if (flackern)
+        {
+            if (Random.value > 0.9)
+            { //a random chance
+                light.enabled = !light.enabled;
+            }
 
-				}
+        }
     }
 
-  
+
 }
