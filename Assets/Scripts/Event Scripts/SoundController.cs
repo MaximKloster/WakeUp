81f31efaec31419da80fe57cc8db95c271c1d9ea
ChristophHoltmann <christoph.holtmann@live.de>
2014-06-24 10:37:10 +0200

    new door

---
+++ Assets/Scripts/Event Scripts/SoundController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource eventSounds;
+
+    [SerializeField]
+    int eventCount = 1;
+
+    bool soundActive = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (eventCount == 0) transform.FindChild("Event Activate Trigger").gameObject.SetActive(false);
+
+        if (soundActive && !eventSounds.isPlaying)
+        {
+            eventSounds.Play();
+            soundActive = false;
+            eventCount--;
+        }
+
+    }
+
+    public void ActivateEvent()
+    {
+        soundActive = true;
+    }
+}
---
+++ Assets/Scripts/Event Scripts/SoundTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTrigger : MonoBehaviour 
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") transform.parent.GetComponent<SoundController>().ActivateEvent();
+    }
+}
