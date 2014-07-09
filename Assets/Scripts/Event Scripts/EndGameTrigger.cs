66868251ac363d41fc2dc75b02af4177944799a7
ChristophHoltmann <christoph.holtmann@live.de>
2014-07-09 14:01:08 +0200

    end trigger

--- Assets/Scripts/Event Scripts/EndGameTrigger.cs
+++ Assets/Scripts/Event Scripts/EndGameTrigger.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class EndGameTrigger : MonoBehaviour
 {
+    bool endGame;
+    Transform player;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-
+            player = other.transform;
+            endGame = true;
         }
+    }
+
+    void Update()
+    {
+        if(endGame)
+            player.FindChild("End Sprite").GetComponent<SpriteRenderer>().color = Color.Lerp(player.FindChild("End Sprite").GetComponent<SpriteRenderer>().color, new Color(1, 1, 1, 1), 1f * Time.deltaTime);
     }
 }
