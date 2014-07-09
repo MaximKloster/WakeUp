20e8b0d00bb2e740a5b70b2462789875856d919d
ChristophHoltmann <christoph.holtmann@live.de>
2014-07-09 12:02:11 +0200

    vqwew

---
+++ Assets/Scripts/Event Scripts/EndGameTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+        }
+    }
+}
