f3e5633e1a9dd4b13313759cc5d2a5e3925caecf
MaximKloster <kloster.maxim@web.de>
2014-07-09 14:09:00 +0200

    sdf

--- Assets/Scripts/Switch Object Scripts/SwitchObjectController.cs
+++ Assets/Scripts/Switch Object Scripts/SwitchObjectController.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class SwitchObjectController : MonoBehaviour
 {
     // variables
+    [SerializeField]
+    GameObject moveableObject = null;
+    [SerializeField]
+    bool bla;
     Transform startPosition, secondPosition, thirdPosition, switchObject;
 
     // Use this for initialization
     void Start()
     {
+        //controllerObject = Instantiate(controller, GameObject.FindGameObjectWithTag("Respawn").transform.position, GameObject.FindGameObjectWithTag("Respawn").transform.rotation) as GameObject;
+
         switchObject = transform.FindChild("Switch Object").transform;
         //startPosition = switchObject;
         secondPosition = transform.FindChild("Second Position").transform;
