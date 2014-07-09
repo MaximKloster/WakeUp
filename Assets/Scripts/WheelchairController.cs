db8a9f9d0434212ef541b6ef22e7b47b29f166ab
ChristophHoltmann <christoph.holtmann@live.de>
2014-07-09 11:05:11 +0200

    flashlight

--- Assets/Scripts/WheelchairController.cs
+++ Assets/Scripts/WheelchairController.cs
@@ -371,6 +371,7 @@ public class WheelchairController : MonoBehaviour
                 if (look)
                 {
                     lookAtObject.GetComponentInParent<DoorController>().OpenDoor();
+
                 }
                 else
                 {
