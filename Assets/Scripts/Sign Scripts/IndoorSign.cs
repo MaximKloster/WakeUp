565d20cc73ab0027630240eef67048501cd0492a
MaximKloster <kloster.maxim@web.de>
2014-07-31 16:06:33 +0200

    many test stuff

---
+++ Assets/Scripts/Sign Scripts/IndoorSign.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IndoorSign : MonoBehaviour 
+{
+    [SerializeField]
+    Texture background = null, arrow = null;
+    [SerializeField]
+    Font textStyle = null;
+    [SerializeField]
+    bool arrow_right = false, arrow_left = false;
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
