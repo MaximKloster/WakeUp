303267ca6d5a2b5d47434b920b0823f1153d348d
MarkusWeigel <markusandreasweigel@gmail.com>
2014-08-01 11:19:21 +0200

    level performance (reverted from commit 1e66f0649474e529d4b1ba6cb52a6080e38cb1ba)

--- Assets/Scripts/WheelchairController.cs
+++ Assets/Scripts/WheelchairController.cs
@@ -394,7 +394,7 @@ public class WheelchairController : MonoBehaviour
 
                             if (eyeRaycastObject.raycastObject.tag == "Door")
                             {
-                                SetEmissionGainOfDoor(0.2f);
+                                SetEmissionGainOfDoor(0.15f);
                                 MoveDoorhandle(true);
                             }
                             else if (eyeRaycastObject.raycastObject.tag == "Flashlight"
