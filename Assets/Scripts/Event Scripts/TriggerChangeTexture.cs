fd3e28420277c375b05bff5b0328c205bbba806f
ChristophHoltmann <christoph.holtmann@live.de>
2014-08-06 11:13:09 +0200

    change texture trigger

---
+++ Assets/Scripts/Event Scripts/TriggerChangeTexture.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerChangeTexture : MonoBehaviour
+{
+    [SerializeField]
+    Texture changeToTexture = null;
+
+    Texture startTexture;
+
+    void Start()
+    {
+        startTexture = renderer.material.mainTexture;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        renderer.material.mainTexture = changeToTexture;
+    }
+}
