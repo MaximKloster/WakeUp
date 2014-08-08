071251efa63c9d7bb7079c1c5f49a7f9d887ab3b
ChristophHoltmann <christoph.holtmann@live.de>
2014-08-08 15:38:43 +0200

    little changes

--- Assets/Scripts/Event Scripts/AttackEvent.cs
+++ Assets/Scripts/Event Scripts/AttackEvent.cs
@@ -25,7 +25,7 @@ public class AttackEvent : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(1.5f);
             player.GetComponent<WheelchairController>().SetLifeStatus(-1);
         }
     }
